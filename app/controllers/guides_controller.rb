79ca6853d7e9ed062251b9bfcbab86c2550b1a39
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-10-12 14:52:44 -0400

    add small changes to drop down

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -4,4 +4,25 @@ class GuidesController < ApplicationController
   def index
     @guide = Guide.where(state: params[:state], payer_name: params[:payer_name], name: params[:name] ).last
   end
+  
+  def new
+    @guide = Guide.new
+    
+  end
+  
+  def create
+    @guide = Guide.new(guide_params)
+    if @guide.save
+      redirect_to root_path
+    else
+      
+    end
+  end
+  
+  protected
+  
+  def guide_params
+    params.require(:guide).permit(:state, :name, :contact, :medicaid_ffs, :managed_care_medicaid, :wic, :private_commercial, :medical_food_policy, :formulary_review_date, :formulary, :reimbursement_methodology, :payer_name, :current_link, :link_notes, :phone_number, :fax_number)
+  end
+  
 end
