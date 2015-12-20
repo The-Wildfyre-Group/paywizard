2aef83785798b187d782df98dfacd7e1e3ae788a
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-12-20 16:12:07 -0500

    create guides table

--- db/migrate/20151002222825_create_guides.rb
+++ db/migrate/20151002222825_create_guides.rb
@@ -3,25 +3,72 @@ class CreateGuides < ActiveRecord::Migration
     create_table :guides do |t|
       t.string :state
       t.string :name
+      t.string :payer
+        
+    # contact
       t.string :contact
-      t.integer :medicaid_ffs
-      t.integer :managed_care_medicaid
-      t.integer :medicaid
-      t.integer :wic
-      t.integer :private_commercial
-      t.integer :uninsured
-      t.integer :medicare_other_public
-      t.string :medical_food_policy
-      t.date   :formulary_review_date
-      t.boolean :formulary
-      t.string :reimbursement_methodology
-      t.string :payer_name
-      t.string :current_link
-      t.text :link_notes
       t.string :phone_number
       t.string :fax_number
+      
+    # coverage
+      # coverage - published policies
+      t.boolean :published_policies
+      t.string  :policy_name
+      t.string  :policy_number
+      t.string  :policy_link
+      t.text    :codes_covered
+      t.text    :approved_diagnoses
+      t.text    :age_limitations
+      t.text    :other_limitations
+      t.boolean :state_mandate_apply
+      t.boolean :major_medical_benefit
+      t.boolean :rx_benefit
+      
+      # coverage - PA required
+      t.boolean :pa_required
+      t.boolean :pa_form_required
+      t.string  :pa_link
+      t.text    :pa_process
+      
+      # coverage - other notes
+      t.text    :coverage_notes
+      
+    # coding
+      t.text    :accepted_billing_codes
+      t.boolean :bo_modifier
+      t.boolean :is_s9433
+      t.string  :coding_link
+      
+    # formulary
+      t.string  :nutricia_formulary_products
+      t.string  :open_or_closed_formulary
+      t.boolean :formulary_exception_allowed
+      t.boolean :formulary_form_required
+      t.string  :formulary_link
+      t.boolean :formulary_documentation_required
+      t.text    :formulary_process
+    
+    # reimbursement
+      t.boolean :reimbursement_fee_schedule
+      t.string  :reimbursement_link
+      t.string  :product_reimbursement_rate
+      t.text    :reimbursement_specific
+      t.text   :reimbursement_methodology
+      t.string  :reimbursement_fees
+      
+    
+    # relationship to other payers
+      t.text :relationship_to_wic
+      t.text :relationship_to_state_medicaid
+      t.text :relationship_to_other_programs
+     
+      t.string :slug
 
       t.timestamps
     end
+    add_index :guides, :name
+    add_index :guides, :payer
+    add_index :guides, :state
+    add_index :guides, :slug
   end
 end
