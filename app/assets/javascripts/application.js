cbb8525f2a525fb2fec96d073880185ab10eedee
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-12-22 12:29:00 -0500

    rm extra jquery from application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require jquery.remotipart
-
 	
 // common functions 
 //= require altair/common.min
